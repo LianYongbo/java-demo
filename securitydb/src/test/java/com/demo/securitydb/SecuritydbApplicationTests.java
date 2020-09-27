89ee00be104c9c6218faa505c9b3af91354cda18
Lian <L1anyX@126.com>
2020-09-27 16:20:45 +0800

    进行了测试和代码修改

--- securitydb/src/test/java/com/demo/securitydb/SecuritydbApplicationTests.java
+++ securitydb/src/test/java/com/demo/securitydb/SecuritydbApplicationTests.java
@@ -3,13 +3,9 @@ package com.demo.securitydb;
 import com.demo.securitydb.dao.RoleDao;
 import com.demo.securitydb.dao.UserDao;
 import com.demo.securitydb.dao.User_RoleDao;
-import com.demo.securitydb.model.Role;
-import com.demo.securitydb.model.User;
-import com.demo.securitydb.model.User_Role;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class SecuritydbApplicationTests {
@@ -26,10 +22,12 @@ class SecuritydbApplicationTests {
     void contextLoads() {
 //        User user1 = new User(1, "lian", new BCryptPasswordEncoder().encode("123"));
 //        User user2 = new User(2, "xin", new BCryptPasswordEncoder().encode("abc"));
-        User user3 = new User(3, "yue", new BCryptPasswordEncoder().encode("abc"));
+//        User user3 = new User(3, "yue", new BCryptPasswordEncoder().encode("abc"));
+//        User user4 = new User(4, "wang", new BCryptPasswordEncoder().encode("abc"));
 //        userDao.saveUser(user1);
 //        userDao.saveUser(user2);
-        userDao.saveUser(user3);
+//        userDao.saveUser(user3);
+//        userDao.saveUser(user4);
 
 //        Role role1 = new Role(1, "admin", "管理员");
 //        Role role2 = new Role(2, "dba", "数据库管理员");
@@ -38,17 +36,17 @@ class SecuritydbApplicationTests {
 //        roleDao.saveRole(role2);
 //        roleDao.saveRole(role3);
 
-        User_Role ur = new User_Role(1, 1, 1);
-        User_Role ur1 = new User_Role(2, 1, 3);
-        User_Role ur2 = new User_Role(3, 2, 2);
-        User_Role ur3 = new User_Role(4, 2, 3);
-        User_Role ur4 = new User_Role(5, 3, 3);
-
-        user_roleDao.saveRecording(ur);
-        user_roleDao.saveRecording(ur1);
-        user_roleDao.saveRecording(ur2);
-        user_roleDao.saveRecording(ur3);
-        user_roleDao.saveRecording(ur4);
+//        User_Role ur = new User_Role(1, 1, 1);
+//        User_Role ur1 = new User_Role(2, 1, 3);
+//        User_Role ur2 = new User_Role(3, 2, 2);
+//        User_Role ur3 = new User_Role(4, 2, 3);
+//        User_Role ur4 = new User_Role(5, 3, 3);
+//
+//        user_roleDao.saveRecording(ur);
+//        user_roleDao.saveRecording(ur1);
+//        user_roleDao.saveRecording(ur2);
+//        user_roleDao.saveRecording(ur3);
+//        user_roleDao.saveRecording(ur4);
     }
 
 }
